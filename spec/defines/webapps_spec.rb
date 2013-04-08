f317fda5cdedbcb0a61a12be5f0686db2590ee02
Yanis Guenane <yguenane@gmail.com>
2013-04-08 16:38:26 +0000

    Bunch of additions

---
+++ spec/defines/webapps_spec.rb
@@ -0,0 +1,26 @@
+# spec/defines/webapps_spec.rb
+
+
+require 'spec_helper'
+
+describe 'pentaho::webapps' do
+	
+	let(:title) { 'puppet' }
+
+	it 'Install pentaho web app in temp directory' do
+
+	end
+
+	it 'Edit WEB-INF/web.xml' do
+
+	end
+
+	it 'Edit WEB-INF/classes/log4j.xml' do
+
+	end
+
+	it 'Edit META-INF/context.xml' do
+
+	end
+
+end
